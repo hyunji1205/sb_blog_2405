3beddeed9bc57b80fe21ed73046897159111a84c
hyunji1205 <sguswl1205@gmail.com>
2024-05-23 17:13:29 +0900

    Member MVC 셋팅, Post author 추가

--- src/main/java/com/example/blog/domain/post/entity/Post.java
+++ src/main/java/com/example/blog/domain/post/entity/Post.java
@@ -1,7 +1,9 @@
 package com.example.blog.domain.post.entity;
 
+import com.example.blog.domain.member.entity.Member;
 import com.example.blog.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,4 +19,6 @@ import lombok.experimental.SuperBuilder;
 public class Post extends BaseEntity {
     private String title;
     private String content;
+    @ManyToOne
+    private Member author;
 }
