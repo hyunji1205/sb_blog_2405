63ed606ad179c41b2865019cfd1cc700b5425421
hyunji1205 <sguswl1205@gmail.com>
2024-05-29 16:04:19 +0900

    사진파일 등록 완료

--- src/main/java/com/example/blog/domain/post/entity/Post.java
+++ src/main/java/com/example/blog/domain/post/entity/Post.java
@@ -3,11 +3,9 @@ package com.example.blog.domain.post.entity;
 import com.example.blog.domain.member.entity.Member;
 import com.example.blog.global.jpa.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
@@ -21,4 +19,5 @@ import java.time.LocalDateTime;
 public class Post extends BaseEntity {
     private String title;
     private String content;
+    private String thumnailImg;
 }
